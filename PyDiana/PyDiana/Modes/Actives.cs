dd3e9a2b1fa295288cb7d74a27eecdd7a3cb69a3
FlyTy14 <teliinfowin@gmail.com>
2017-02-18 15:05:54 -0200

    PyDiana "2.0"

---
+++ PyDiana/PyDiana/Modes/Actives.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+using PyDiana;
+
+namespace PyDiana.Modes
+{
+
+    class Actives
+    {
+        private static AIHeroClient Player { get { return ObjectManager.Player; } }
+        public static AIHeroClient _Player
+        {
+            get { return ObjectManager.Player; }
+        }
+        public static Menu Menu, PyCombo, PyHarass, KillSteal, Lane, Jungle;
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            switch (Orbwalker.ActiveModesFlags)
+            {
+                case Orbwalker.ActiveModes.Combo:
+                    Combo();
+                    break;
+                case Orbwalker.ActiveModes.Harass:
+                    Harass();
+                    break;
+                case Orbwalker.ActiveModes.LaneClear:
+                    LaneClear();
+                    break;
+                case Orbwalker.ActiveModes.JungleClear:
+                    JungleClear();
+                    break;
+                case Orbwalker.ActiveModes.LastHit:
+                    break;
+                case Orbwalker.ActiveModes.None:
+                    break;
+            }
+            
+        }
+
+    private static void Combo()
+        {
+            var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
+
+            if (target == null)
+                return;
+            if (PyCombo["UseQ"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.IsInRange(Player, Q.Range) && Q.IsReady())
+                {
+
+                    if (Q.GetPrediction(target).HitChance >= HitChance.High)
+                    {
+                        Q.Cast(Q.GetPrediction(target).CastPosition);
+                    }
+
+
+                }
+            }
+            if (PyCombo["UseW"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.IsInRange(Player, W.Range) && W.IsReady())
+                {
+
+                    W.Cast();
+
+
+
+                }
+            }
+            if (PyCombo["UseE"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.IsInRange(Player, E.Range) && E.IsReady())
+                {
+
+                    E.Cast();
+
+
+                }
+            }
+            if (PyCombo["UseR"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.IsInRange(Player, R.Range) && !Q.IsReady()) 
+                {
+
+                    R.Cast(target);
+
+
+
+                }
+            }
+
+
+        }
+        private static void Harass()
+        {
+            var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
+
+            if (target == null)
+                return;
+            if (PyHarass["UseQ"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.IsInRange(Player, Q.Range) && Q.IsReady())
+                {
+
+                    if (Q.GetPrediction(target).HitChance >= HitChance.High)
+                    {
+                        Q.Cast(Q.GetPrediction(target).CastPosition);
+                    }
+
+
+                }
+            }
+            if (PyHarass["UseW"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.IsInRange(Player, W.Range) && W.IsReady())
+                {
+
+                    W.Cast();
+
+
+
+                }
+            }
+
+
+
+        }
+
+        private static void LaneClear()
+        {
+            if (Lane["Qclearmana"].Cast<Slider>().CurrentValue <= Player.ManaPercent)
+            {
+                var minion1 = EntityManager.MinionsAndMonsters.EnemyMinions.FirstOrDefault(m => m.IsValidTarget(Q.Range));
+
+                Q.Cast(minion1);
+
+            }
+            if (Lane["Wclearmana"].Cast<Slider>().CurrentValue <= Player.ManaPercent)
+            {
+                var minion1 = EntityManager.MinionsAndMonsters.EnemyMinions.FirstOrDefault(m => m.IsValidTarget(W.Range));
+
+                W.Cast();
+
+            }
+
+        }
+        private static void JungleClear()
+        {
+
+            if (Lane["Qclearjg"].Cast<CheckBox>().CurrentValue)
+            {
+                var monster = EntityManager.MinionsAndMonsters.GetJungleMonsters(Player.Position, Q.Range);
+                Q.Cast(monster.First());
+            }
+            if (Lane["Wclearjg"].Cast<CheckBox>().CurrentValue)
+            {
+                var monster = EntityManager.MinionsAndMonsters.GetJungleMonsters(Player.Position, W.Range);
+                W.Cast();
+            }
+            if (Lane["Rclearjg"].Cast<CheckBox>().CurrentValue)
+            {
+                var monster = EntityManager.MinionsAndMonsters.Monsters.FirstOrDefault(m => m.IsValidTarget(R.Range));
+                R.Cast(monster);
+            }
+
+        }
+    }
+}
+
+    
+
---
+++ PyDiana/PyDiana/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+using PyDiana.Modes;
+
+namespace PyDiana
+{
+    class Program
+    {
+        public static AIHeroClient _Player
+        {
+            get { return ObjectManager.Player; }
+        }
+        public static Menu Menu, PyCombo, PyHarass, KillSteal, Lane, Jungle;
+
+        //Remind me to update this
+        public static Spell.Skillshot Q;
+        //Remind me to update this
+        public static Spell.Active W;
+        //Remind me to update this
+        public static Spell.Active E;
+        //Remind me to update this
+        public static Spell.Targeted R, R2;
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            Q = new Spell.Skillshot(SpellSlot.Q, 825, SkillShotType.Linear);
+            {
+                Q.AllowedCollisionCount = int.MaxValue;
+
+            }
+            W = new Spell.Active(SpellSlot.W, 250);
+            E = new Spell.Active(SpellSlot.E, 350);
+            R = new Spell.Targeted(SpellSlot.R, 825);
+
+            Menu = MainMenu.AddMenu("Diana", "Diana 2.0");
+
+            PyCombo = Menu.AddSubMenu("Combo");
+            //combo
+            PyCombo.Add("comboQ", new CheckBox("Use Q ", true));
+            PyCombo.Add("comboW", new CheckBox("Use W ", true));
+            PyCombo.Add("comboE", new CheckBox("Use E ", true));
+            PyCombo.Add("comboR", new CheckBox("Use R", true));
+
+            PyHarass = Menu.AddSubMenu("Harass");
+            //Harass
+            PyHarass.Add("Q", new CheckBox("Use Q"));
+            PyHarass.Add("W", new CheckBox("Use W"));
+            PyHarass.Add("E", new CheckBox("Use E", false));
+            PyHarass.Add("ManaPercent", new Slider("Minimum Mana Percent", 25));
+
+            KillSteal = Menu.AddSubMenu("KillSteal");
+            //KS
+            KillSteal.Add("Q", new CheckBox("Use Q"));
+            KillSteal.Add("W", new CheckBox("Use W"));
+            KillSteal.Add("R", new CheckBox("Use R"));
+
+            Lane = Menu.AddSubMenu("LaneClear");
+            //LaneClear
+            Lane.Add("LaneClear.Q", new Slider("Use Q >= {0}", 4, 0, 10));
+            Lane.Add("LaneClear.W", new Slider("Use W >= {0}", 3, 0, 10));
+            Lane.Add("LaneClear.ManaPercent", new Slider("Minimum Mana Percent", 50));
+
+            Jungle = Menu.AddSubMenu("Jungleclear");
+            //Jungleclear
+            Jungle.Add("JungleClear.Q", new CheckBox("Use Q"));
+            Jungle.Add("JungleClear.W", new CheckBox("Use W"));
+            Jungle.Add("JungleClear.ManaPercent", new Slider("Minimum Mana Percent", 20));
+        }
+    }
+}
